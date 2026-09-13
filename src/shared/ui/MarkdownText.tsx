60b45f506358adf4274d20e31e04caac31dd29aa
Shiyue137 <shiyue137mh@gmail.com>
2026-09-14 00:58:56 +0800

    Use inline Discord icon for trusted markdown links

--- src/shared/ui/MarkdownText.tsx
+++ src/shared/ui/MarkdownText.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { getUrlSafetyInfo, parseHttpUrl } from '@/shared/lib/urlSafety';
+import { getUrlSafetyInfo, isTrustedDiscordHostname, parseHttpUrl } from '@/shared/lib/urlSafety';
 import { ExternalLinkWarningDialog } from '@/shared/ui/ExternalLinkWarningDialog';
 
 interface MarkdownTextProps {
@@ -43,8 +43,10 @@ function buildSafeAnchor(label: string, rawUrl: string): string {
   }
 
   const href = escapeHtmlAttribute(parsedUrl.toString());
-  const favicon = escapeHtmlAttribute(`${parsedUrl.origin}/favicon.ico`);
-  return `<a href="${href}" target="_blank" rel="noopener noreferrer" class="discord-link"><span aria-hidden="true" class="link-favicon" style="background-image:url('${favicon}')"></span>${label}</a>`;
+  const faviconMarkup = isTrustedDiscordHostname(parsedUrl.hostname)
+    ? '<span aria-hidden="true" class="link-favicon link-favicon-discord"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 0-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 0 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 0 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 1-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.082.082 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z"/></svg></span>'
+    : `<span aria-hidden="true" class="link-favicon" style="background-image:url('${escapeHtmlAttribute(`${parsedUrl.origin}/favicon.ico`)}')"></span>`;
+  return `<a href="${href}" target="_blank" rel="noopener noreferrer" class="discord-link">${faviconMarkup}${label}</a>`;
 }
 
 /**
